3a751c4a365a0cba950aa69e253fafde6f14583d
Bashu-011 <bashu2609@gmail.com>
2024-01-04 10:37:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(drivers, name){
+    const lowerName = name.toLowerCase()
+    return drivers.filter(driver => {
+        const lowerDriver = driver.toLowerCase()
+        return lowerDriver === lowerName}
+        )
+}
+
+function fuzzyMatch(drivers, name){
+    const lowerCaseName = name.toLowerCase();
+
+  return drivers.filter(driver => {
+    const lowerCaseDriverName = driver.toLowerCase();
+    const beginningLetters = lowerCaseDriverName.slice(0, lowerCaseName.length);
+    return beginningLetters === lowerCaseName;
+  });
+}
+
+function matchName(drivers, name){
+    const lowerName = name.toLowerCase()
+    return drivers.filter(driver => {
+        const lowerDriver = driver.name.toLowerCase()
+        return lowerDriver ===lowerName})
+}
